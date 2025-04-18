08b69a804c39756d52ab40a6892b9ab14a3b4186
deverzius <nguyenmanh.4949@gmail.com>
2025-04-18 10:27:37 +0700

    Fix: Re-init Admin Site project

--- src/CommerceCore.Web/AdminSite/vite.config.ts
+++ src/CommerceCore.Web/AdminSite/vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { reactRouter } from "@react-router/dev/vite";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
