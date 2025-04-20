702bee6498df2ad64955b101ff66a781dfb2e197
deverzius <nguyenmanh.4949@gmail.com>
2025-04-20 14:12:08 +0700

    Fix: Re-init Customers Site project

---
+++ src/CommerceCore.Web/CustomersSite/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CustomersSite.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
