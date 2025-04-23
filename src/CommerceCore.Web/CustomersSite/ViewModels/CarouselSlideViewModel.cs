5e72b3859f0d7fa2ce22b4d9cdc229ed56ac195e
deverzius <nguyenmanh.4949@gmail.com>
2025-04-23 16:19:14 +0700

    Fix: Remove hardcode & Update to render Carousel dynamically in Customers Site

---
+++ src/CommerceCore.Web/CustomersSite/ViewModels/CarouselSlideViewModel.cs
@@ -0,0 +1,11 @@
+namespace CustomersSite.ViewModels;
+
+public class CarouselSlideViewModel
+{
+    public string ImgSrc { get; set; }
+    public string Title { get; set; }
+    public string Content { get; set; }
+    public string CtaUrl { get; set; }
+    public string CaptionPosition { get; set; }
+    public bool IsDarkText { get; set; }
+}
