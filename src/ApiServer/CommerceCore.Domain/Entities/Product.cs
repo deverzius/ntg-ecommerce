e4ba0bde8e4f8e64e0ad6ae322c132a1cf0612aa
deverzius <nguyenmanh.4949@gmail.com>
2025-04-26 16:32:30 +0700

    Chore: Move Product model to separated file in Api Server

---
+++ src/ApiServer/CommerceCore.Domain/Entities/Product.cs
@@ -0,0 +1,38 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CommerceCore.Domain.Entities;
+
+public class Product
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public Guid Id { get; set; }
+
+    [Required]
+    [StringLength(100)]
+    public required string Name { get; set; }
+
+    [Required]
+    [StringLength(500)]
+    public required string Description { get; set; }
+
+    [Required]
+    public required decimal Price { get; set; }
+
+    [Required]
+    public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+
+    [Required]
+    public DateTime UpdatedDate { get; set; } = DateTime.UtcNow;
+
+    [Required]
+    public required Guid BrandId { get; set; }
+
+    [ForeignKey("BrandId")]
+    public virtual Brand Brand { get; set; }
+    public virtual ICollection<ProductImage> Images { get; set; } = [];
+    public virtual ICollection<ProductVariant> Variants { get; set; } = [];
+    public virtual ICollection<Category> Categories { get; set; } = [];
+    public virtual ICollection<Review> Reviews { get; set; } = [];
+}
