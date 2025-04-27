e58d3ca7d4e08c9a83f67e68366e23021405be55
deverzius <nguyenmanh.4949@gmail.com>
2025-04-27 14:19:30 +0700

    Fix: Prevent ParentCategoryId from being equal to CategoryId when updating Category in API Server

--- src/ApiServer/CommerceCore.WebApi/Controllers/v1/ProductsController.cs
+++ src/ApiServer/CommerceCore.WebApi/Controllers/v1/ProductsController.cs
@@ -2,71 +2,71 @@
 using CommerceCore.Application.Common.Interfaces;
 using CommerceCore.Application.Dtos.ProductDto;
 
-namespace CommerceCore.WebApi.Controllers.v1
+namespace CommerceCore.WebApi.Controllers.v1;
+
+[ApiController]
+[ApiVersion("1.0")]
+[Route("api/v{version:apiVersion}/[controller]")]
+public class ProductsController(IProductService productService) : ControllerBase
 {
-    [ApiController]
-    [ApiVersion("1.0")]
-    [Route("api/v{version:apiVersion}/[controller]")]
-    public class ProductsController(IProductService productService) : ControllerBase
+    private readonly IProductService _productService = productService;
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<ProductResponseDto>>> GetProducts()
     {
-        private readonly IProductService _productService = productService;
+        return Ok(await _productService.GetAllAsync());
+    }
 
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<ProductResponseDto>>> GetProducts()
+    [HttpGet("{id}")]
+    public async Task<ActionResult<ProductResponseDto>> GetProduct(Guid id)
+    {
+        var product = await _productService.GetByIdAsync(id);
+
+        if (product == null)
         {
-            return Ok(await _productService.GetAllAsync());
+            return NotFound();
         }
 
-        [HttpGet("{id}")]
-        public async Task<ActionResult<ProductResponseDto>> GetProduct(Guid id)
+        return Ok(product);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutProduct(Guid id, UpdateProductRequestDto product)
+    {
+        var updatedProduct = await _productService.UpdateAsync(id, product);
+
+        if (updatedProduct == null)
         {
-            var product = await _productService.GetByIdAsync(id);
-
-            if (product == null)
-            {
-                return NotFound();
-            }
-
-            return Ok(product);
+            return BadRequest();
         }
 
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutProduct(Guid id, UpdateProductRequestDto product)
+        return Ok(updatedProduct);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<ProductResponseDto>> PostProduct(CreateProductRequestDto productDto)
+    {
+        var createdProductDto = await _productService.CreateAsync(productDto);
+
+        if (createdProductDto == null)
         {
-            var updatedProduct = await _productService.UpdateAsync(id, product);
-
-            if (updatedProduct == null)
-            {
-                return BadRequest();
-            }
-
-            return Ok(updatedProduct);
+            return BadRequest();
         }
 
-        [HttpPost]
-        public async Task<ActionResult<ProductResponseDto>> PostProduct(CreateProductRequestDto productDto)
+        return CreatedAtAction(nameof(GetProduct), new { id = createdProductDto.Id }, createdProductDto);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteProduct(Guid id)
+    {
+        var isDeleted = await _productService.DeleteAsync(id);
+
+        if (!isDeleted)
         {
-            var createdProductDto = await _productService.CreateAsync(productDto);
-
-            if (createdProductDto == null)
-            {
-                return BadRequest();
-            }
-
-            return CreatedAtAction(nameof(GetProduct), new { id = createdProductDto.Id }, createdProductDto);
+            return BadRequest();
         }
 
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteProduct(Guid id)
-        {
-            var isDeleted = await _productService.DeleteAsync(id);
-
-            if (!isDeleted)
-            {
-                return BadRequest();
-            }
-
-            return Ok();
-        }
+        return Ok();
     }
 }
+
