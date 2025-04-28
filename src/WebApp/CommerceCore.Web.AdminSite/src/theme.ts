f7b85f407b451df911649555888f20f4a497dd08
deverzius <nguyenmanh.4949@gmail.com>
2025-04-28 11:26:53 +0700

    Fix: Update theme for Table, Title and Paper components of Admin Site

--- src/WebApp/CommerceCore.Web.AdminSite/src/theme.ts
+++ src/WebApp/CommerceCore.Web.AdminSite/src/theme.ts
@@ -1,4 +1,5 @@
-import { createTheme, rem } from "@mantine/core";
+import { createTheme, rem, type MantineTheme } from "@mantine/core";
+import { FontWeight } from "./types/enum";
 
 export const theme = createTheme({
   fontSizes: {
@@ -7,5 +8,42 @@ export const theme = createTheme({
     md: rem(14),
     lg: rem(16),
     xl: rem(20),
+  },
+
+  components: {
+    Paper: {
+      styles: () => ({
+        bg: "#FFFFFF",
+        root: {
+          overflow: "hidden",
+        },
+      }),
+    },
+    Table: {
+      defaultProps: {
+        verticalSpacing: "sm",
+        horizontalSpacing: "xl",
+        borderColor: "gray.1",
+      },
+      styles: (theme: MantineTheme) => ({
+        thead: {
+          backgroundColor: theme.colors.gray[0],
+        },
+        th: {
+          fontSize: theme.fontSizes.md,
+          fontWeight: FontWeight.Medium,
+          color: theme.colors.gray[8],
+        },
+        td: {
+          fontSize: theme.fontSizes.md,
+          color: theme.colors.gray[9],
+        },
+      }),
+    },
+    Title: {
+      defaultProps: {
+        size: "h3",
+      },
+    },
   },
 });
