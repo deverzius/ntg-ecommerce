f4e2756b4bf3e7dc830c011c5847a101044632d7
deverzius <nguyenmanh.4949@gmail.com>
2025-04-29 17:41:17 +0700

    Feat: Add Brand DTOs to Admin Site

---
+++ src/WebApp/CommerceCore.Web.AdminSite/src/types/dtos/brand/request.ts
@@ -0,0 +1,10 @@
+export type CreateBrandRequestDto = {
+  name: string;
+  description: string;
+};
+
+export type UpdateBrandRequestDto = {
+  id: string;
+  name: string;
+  description: string;
+};
