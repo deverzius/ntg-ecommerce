f08407826d40045b01716b666d382bf45b881cce
deverzius <nguyenmanh.4949@gmail.com>
2025-05-01 22:50:39 +0700

    Chore: Remove unused code in Api Server

--- src/ApiServer/CommerceCore.Application/DependencyInjection.cs
+++ src/ApiServer/CommerceCore.Application/DependencyInjection.cs
@@ -7,9 +7,6 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
-        // services.AddScoped<IProductService, ProductService>();
-        // services.AddScoped<ICategoryService, CategoryService>();
-        // services.AddScoped<IBrandService, BrandService>();
         services.AddMediatR(cfg =>
             cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly())
         );
