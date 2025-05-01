7ed5421efbb952f5e6096a2f1e20b3cb85e96590
deverzius <nguyenmanh.4949@gmail.com>
2025-05-01 11:44:47 +0700

    Fix: Update Brands controller to follow Command Pattern on API Server

--- src/ApiServer/CommerceCore.WebApi/Controllers/v1/BrandsController.cs
+++ src/ApiServer/CommerceCore.WebApi/Controllers/v1/BrandsController.cs
@@ -1,72 +1,37 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using CommerceCore.Application.Common.Interfaces;
-using CommerceCore.Application.Dtos.BrandDto;
+﻿using CommerceCore.Application.Brands.Dtos;
+using CommerceCore.Application.Brands.Queries.GetBrand;
+using CommerceCore.Application.Brands.Queries.GetBrandsWithPagination;
+using CommerceCore.Application.Common.Models;
+using MediatR;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CommerceCore.WebApi.Controllers.v1;
 
 [ApiController]
 [ApiVersion("1.0")]
 [Route("api/v{version:apiVersion}/[controller]")]
-public class BrandsController(IBrandService brandService) : ControllerBase
+public class BrandsController() : ControllerBase
 {
-    private readonly IBrandService _brandService = brandService;
-
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<BrandResponseDto>>> GetBrands()
+    [ProducesResponseType(typeof(Ok<PaginatedList<BrandResponseDto>>), StatusCodes.Status200OK)]
+    public async Task<Ok<PaginatedList<BrandResponseDto>>> GetBrandsWithPagination(
+        ISender sender,
+        [FromQuery] GetBrandsWithPaginationQuery query
+    )
     {
-        return Ok(await _brandService.GetAllAsync());
+        var result = await sender.Send(query);
+
+        return TypedResults.Ok(result);
     }
 
     [HttpGet("{id}")]
-    public async Task<ActionResult<BrandResponseDto>> GetBrand(Guid id)
+    [ProducesResponseType(typeof(Ok<BrandResponseDto>), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<Results<Ok<BrandResponseDto>, NotFound>> GetBrand(ISender sender, Guid id)
     {
-        var brand = await _brandService.GetByIdAsync(id);
+        var result = await sender.Send(new GetBrandQuery(id));
 
-        if (brand == null)
-        {
-            return NotFound();
-        }
-
-        return Ok(brand);
-    }
-
-    [HttpPut("{id}")]
-    public async Task<IActionResult> PutBrand(Guid id, UpdateBrandRequestDto brandDto)
-    {
-        var updatedBrand = await _brandService.UpdateAsync(id, brandDto);
-
-        if (updatedBrand == null)
-        {
-            return BadRequest();
-        }
-
-        return Ok(updatedBrand);
-    }
-
-    [HttpPost]
-    public async Task<ActionResult<BrandResponseDto>> PostBrand(CreateBrandRequestDto brandDto)
-    {
-        var createdBrandDto = await _brandService.CreateAsync(brandDto);
-
-        if (createdBrandDto == null)
-        {
-            return BadRequest();
-        }
-
-        return CreatedAtAction(nameof(GetBrand), new { id = createdBrandDto.Id }, createdBrandDto);
-    }
-
-    [HttpDelete("{id}")]
-    public async Task<IActionResult> DeleteBrand(Guid id)
-    {
-        var isDeleted = await _brandService.DeleteAsync(id);
-
-        if (!isDeleted)
-        {
-            return BadRequest();
-        }
-
-        return Ok();
+        return result == null ? TypedResults.NotFound() : TypedResults.Ok(result);
     }
 }
-
