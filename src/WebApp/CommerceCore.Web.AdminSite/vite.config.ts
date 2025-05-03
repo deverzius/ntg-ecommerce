b730faa2da0cf036f19055c4b31ac63c0f611ba6
deverzius <nguyenmanh.4949@gmail.com>
2025-05-03 23:00:51 +0700

    Feat: Config https for Admin Site

--- src/WebApp/CommerceCore.Web.AdminSite/vite.config.ts
+++ src/WebApp/CommerceCore.Web.AdminSite/vite.config.ts
@@ -3,9 +3,10 @@ import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import mkcert from "vite-plugin-mkcert";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), mkcert()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
