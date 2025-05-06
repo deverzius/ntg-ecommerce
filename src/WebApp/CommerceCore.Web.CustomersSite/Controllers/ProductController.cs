80bd685a5e984346ace362ba6bda91bc1428adcb
deverzius <nguyenmanh.4949@gmail.com>
2025-05-06 22:02:54 +0700

    Feat: Integrate API product details in Customers Site

--- src/WebApp/CommerceCore.Web.CustomersSite/Controllers/ProductController.cs
+++ src/WebApp/CommerceCore.Web.CustomersSite/Controllers/ProductController.cs
@@ -1,13 +1,47 @@
+using System.Text.Json;
+using System.Threading.Tasks;
+using Ardalis.GuardClauses;
+using CommerceCore.Web.CustomersSite.Shared.Helpers;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CommerceCore.Web.CustomersSite.Controllers;
 
 [Route("[controller]/[action]")]
-public class ProductController(ILogger<ProductController> logger) : Controller
+public class ProductController(
+    ILogger<ProductController> logger,
+    HttpClient httpClient,
+    IConfiguration config
+) : Controller
 {
+    private readonly ILogger<ProductController> _logger = logger;
+    private readonly HttpClient _httpClient = httpClient;
+    private readonly string _apiUrl =
+        config["API:BaseUrl"] ?? Guard.Against.NullOrEmpty(config["API:BaseUrl"]);
+
     [HttpGet("{id}")]
-    public IActionResult Details(string id)
+    public async Task<IActionResult> Details(string id)
     {
-        return View();
+        var product = await FetchProductById(id);
+
+        return View(product);
+    }
+
+    private async Task<ProductViewModel?> FetchProductById(string id)
+    {
+        try
+        {
+            var response = await _httpClient.GetAsync(_apiUrl + "/v1/products/" + id);
+            response.EnsureSuccessStatusCode();
+
+            var json = await response.Content.ReadAsStringAsync();
+            var product = JsonSerializer.Deserialize<ProductViewModel>(json, JsonHelper.Options);
+
+            return product;
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, ex.Message);
+            return null;
+        }
     }
 }
