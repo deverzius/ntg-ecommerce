f9f9fc18a796f27514de1176f518dc3f0362a40f
manhngtien <nguyenmanh.4949@gmail.com>
2025-05-31 15:53:07 +0700

    Chore: Add SQL queries to create categories

---
+++ scripts/Init.sql
@@ -0,0 +1,9 @@
+USE CommerceCore;
+
+INSERT INTO Categories (Id, Name, Description)
+VALUES (NEWID(), 'Top', 'Description for top');
+GO
+
+INSERT INTO Categories (Id, Name, Description)
+VALUES (NEWID(), 'Bottom', 'Description for bottom');
+GO
